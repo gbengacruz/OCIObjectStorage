6f5aac5511634438cd5b55461f2e8a3d9b2a8228
Gbenga Cruz <gbenga.cruz@gmail.com>
2023-10-09 16:47:52 +0300

    okay
    okay

--- package/oci_bucket_api.sql
+++ package/oci_bucket_api.sql
@@ -5,20 +5,20 @@ create or replace PACKAGE  oci_bucket_api AS
  
     -- create oci bucket
     procedure create_bucket(p_credential              in varchar2,   
-	                            p_compartmentid       in varchar2,
-	                            p_namespace           in varchar2,
-								p_oci_base            in varchar2,
-								p_oci_bucket_base     in varchar2,
-								p_oci_bucket_expr     in varchar2,
-								p_bucket_name         in varchar2,
-								p_pre_auth            out varchar2);
+							p_compartmentid       in varchar2,
+							p_namespace           in varchar2,
+							p_oci_base            in varchar2,
+							p_oci_bucket_base     in varchar2,
+							p_oci_bucket_expr     in varchar2,
+							p_bucket_name         in varchar2,
+							p_pre_auth            out varchar2);
 	
 	-- upload to oci bucket
 	procedure upload_file(p_credential        in varchar2,
-						  p_oci_bucket_base   in varchar2,
-						  p_bucket_name       in varchar2,
-						  p_file_name         in varchar2,
-						  p_mime_type         in varchar2,
-						  p_file in blob);
+							p_oci_bucket_base   in varchar2,
+							p_bucket_name       in varchar2,
+							p_file_name         in varchar2,
+							p_mime_type         in varchar2,
+							p_file in blob);
 	
 end oci_bucket_api;
--- package/oci_bucket_api_body.sql
+++ package/oci_bucket_api_body.sql
@@ -5,13 +5,13 @@ create or replace PACKAGE BODY  oci_bucket_api AS
  
     -- create oci bucket
     procedure create_bucket(p_credential              in varchar2,
-	                            p_compartmentid       in varchar2,
-	                            p_namespace           in varchar2,
-								p_oci_base            in varchar2,
-								p_oci_bucket_base     in varchar2,
-								p_oci_bucket_expr     in varchar2,
-								p_bucket_name         in varchar2,
-								p_pre_auth            out varchar2) as
+							p_compartmentid       in varchar2,
+							p_namespace           in varchar2,
+							p_oci_base            in varchar2,
+							p_oci_bucket_base     in varchar2,
+							p_oci_bucket_expr     in varchar2,
+							p_bucket_name         in varchar2,
+							p_pre_auth            out varchar2) as
 								
     l_clob 	clob;
     begin   
@@ -55,11 +55,11 @@ create or replace PACKAGE BODY  oci_bucket_api AS
 	
     -- upload to oci bucket
 	procedure upload_file(p_credential        in varchar2,
-						  p_oci_bucket_base   in varchar2,
-						  p_bucket_name       in varchar2,
-						  p_file_name         in varchar2,
-						  p_mime_type         in varchar2,
-						  p_file in blob) as
+							p_oci_bucket_base   in varchar2,
+							p_bucket_name       in varchar2,
+							p_file_name         in varchar2,
+							p_mime_type         in varchar2,
+							p_file in blob) as
 
 	l_request_url varchar2(32767);
 	l_content_length number;
